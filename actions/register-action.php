6237ab16089cc3b3e54a24ce7373508391981f57
unknown <makoto.08262199@gmail.com>
2025-01-17 22:14:11 +0900

    my first upload

---
+++ actions/register-action.php
@@ -0,0 +1,15 @@
+<?php
+
+    include "../classes/User.php";
+
+    # Create an object
+    $user = new User;
+
+    # Call the store function
+    $user->store($_POST);
+    /**
+     * $_POST = [first_name, last_name,username,password]
+     * 
+     */
+
+?>
